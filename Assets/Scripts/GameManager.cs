0a0c13a50b6cb09b3d58f4baed1d0251b6decd25
KokatsuKitsune <kendricksmith@creighton.edu>
2021-05-24 10:41:34 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,18 +5,28 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+    //number of tiles that make up the level for background generation
     public Vector2 levelSize = new Vector2(20f, 15f);
+    //list of sprites for background generation
     public Sprite[] wallTiles;
+    //transform of background
     public Transform tileBase;
+    //parent object that the background tiles are instantiated in
     public GameObject environment;
+    //exit ladder object the scene is looking at
     //public ExitLadder ladder;
+    //is the scene in the process of changing? This helps prevent multiple calls
     //public boolean sceneChanging;
+    //the player game object for use in scene transition
     //public GameObject player;
-
+    //the current level that is loaded 0 for testing >0 are actual levels in numeric order
     public int levelLoaded = 0;
 
     void Start() {
+        //scene has started and thus scene is able to change
         //scenechanging = false;
+        
+        //background generation [ignore]
         for(int i = 0; i < levelSize.x; i++) {
             for(int j = 0; j < levelSize.y; j++) {
                 Transform tile = Instantiate(tileBase, new Vector3(i - levelSize.x/2, j - levelSize.y/2, 7f), Quaternion.identity);
